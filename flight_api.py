2f471ebf173b232aacad834bac9b89426960533a
June Jin <junejin620@gmail.com>
2026-06-16 10:30:38 +0800

    refactor flight api structure

--- flight_api.py
+++ flight_api.py
@@ -1,15 +1,36 @@
+from config import TARGET_DATE
+
+
+def search_route(origin, destination, date):
+    """
+    后续这里接真实数据源
+    """
+    print(f"查询 {origin} -> {destination} {date}")
+
+    mock_data = {
+        ("URC", "CAN"): 1380,
+        ("URC", "SZX"): 980,
+        ("URC", "NNG"): 950,
+        ("URC", "XMN"): 1000,
+        ("URC", "ZUH"): 1100,
+        ("URC", "HAK"): 1200,
+        ("URC", "FOC"): 1250,
+    }
+
+    return mock_data.get((origin, destination))
+
+
 def get_direct_price():
-    return 1380
+    return search_route(
+        "URC",
+        "CAN",
+        TARGET_DATE
+    )
 
 
 def get_hidden_city_price(destination):
-    mock_prices = {
-        "SZX": 980,
-        "ZUH": 1100,
-        "NNG": 950,
-        "HAK": 1200,
-        "FOC": 1250,
-        "XMN": 1000
-    }
-
-    return mock_prices.get(destination)
+    return search_route(
+        "URC",
+        destination,
+        TARGET_DATE
+    )
